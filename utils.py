a75ebcfcd74498b54c00e7ace14214acfbb5e6ee
Anorpe <aorregop@unal.edu.co>
2023-06-07 09:45:21 -0500

    a

--- utils.py
+++ utils.py
@@ -24,35 +24,64 @@ ohe.fit(X.reshape(-1,1))
 
 
 def complete_coding(matrix,max_len):
-    """
+  """
 
-    Tranforma a minusculas todas las columnas de un Dataframe 
+  Complete the matrix that represents a sequence with vectors of zeros until it has a number of rows equal to max_len
 
-    Parametros
-    ----------
-    data: Dataframe
-    Dataframe que se va a procesar
+  parameters
+  ----------
+  matrix: matrix that represents the one hot encoding of a sequence
+  max_len: maximum row length
 
-    Retorna
-    -------
-    df: Dataframe
-    """
-    agg = max_len - len(matrix)
-    zeros = [0]*len(valid_aminoacids)
-    if(type(matrix)!=type([])):
-        matrix = list(matrix)
-    for i in range(agg):
-        matrix.append(zeros)
-    
-    return matrix
+  return
+  -------
+  matrix: matrix
+
+  """
+  agg = max_len - len(matrix)
+  zeros = [0]*len(valid_aminoacids)
+  if(type(matrix)!=type([])):
+      matrix = list(matrix)
+  for i in range(agg):
+      matrix.append(zeros)
+
+  return matrix
+
 
 def complete_sequence(sequence,max_len):
-    agregate = int(max_len - len(sequence))
-    
-    return sequence + "_"*agregate
+  """
+
+  Complete the sequence with "_" character until it has a number of max_len
+
+  parameters
+  ----------
+  sequence: sequence
+  max_len: maximum row length
+
+  return
+  -------
+  sequence: string
+
+  """
+  agregate = int(max_len - len(sequence))
+
+  return sequence + "_"*agregate
 
 
 def escalon(array):
+  """
+
+    Converts an array to a binary array, where all of its values are set to zero except the maximum value which is converted to one
+
+    parameters
+    ----------
+    array: list to transform
+
+    return
+    -------
+    array: array
+
+  """
   maximo = max(array)
   escalon = []
   for i in array:
@@ -63,7 +92,21 @@ def escalon(array):
 
   return np.array(escalon)
 
+
 def escalon_matrix(matrix):
+  """
+
+    Apply the "escalon" function on a matrix
+
+    parameters
+    ----------
+    matrix: matrix to tranform
+
+    return
+    -------
+    matrix: list
+
+  """
   escalon_matrix = []
   for array in matrix:
     escalon_matrix.append(np.array(escalon(array)))
@@ -71,6 +114,19 @@ def escalon_matrix(matrix):
 
 
 def get_input_generator(seq):
+  """
+
+    converts a sequence to an input to the generator
+
+    parameters
+    ----------
+    seq: sequence to transform
+
+    return
+    -------
+    sequence: array
+
+  """
   #complete sequence with _
   seq = complete_sequence(seq,35)
   #coding
@@ -81,21 +137,47 @@ def get_input_generator(seq):
     for j in range(21):
       flatten.append(int(coding[i][j]))
   flatten = np.array(flatten).reshape(1,-1)
-  
+
   return flatten
 
   
 def flatten(coding):
+  """
+
+    Flattens the matrix that encodes the one hot encoding representation of a sequence
+
+    parameters
+    ----------
+    coding: matrix representing a sequence
+
+    return
+    -------
+    flatten: array
+
+  """
   flatten = []
   for i in range(35):
     for j in range(21):
       flatten.append(int(coding[i][j]))
   flatten = np.array(flatten).reshape(1,-1)
-  
+
   return flatten
 
 
 def encoding(data):
+  """
+
+    applies a one hot encoding to a set of sequences
+
+    parameters
+    ----------
+    data: Dataframe containing the sequences
+
+    return
+    -------
+    ohes: array
+
+  """
   sequences = list(complete_sequence(i,35) for i in data['sequence'])
   ohes = []
   for sequence in sequences:
